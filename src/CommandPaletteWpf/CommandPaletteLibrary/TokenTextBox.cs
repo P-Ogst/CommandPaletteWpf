2c0701c0095ea924fdfb6b0d32730b886d5549ee
Ogawa_Satoshi <morino_mirai@yahoo.co.jp>
2020-04-14 16:52:36 +0900

    Add TokenTextBox

---
+++ src/CommandPaletteWpf/CommandPaletteLibrary/TokenTextBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CommandPaletteLibrary
+{
+    public class TokenTextBox : RichTextBox
+    {
+        public DataTemplate TokenTemplate
+        {
+            get { return (DataTemplate)GetValue(TokenTemplateProperty); }
+            set { SetValue(TokenTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty TokenTemplateProperty =
+            DependencyProperty.Register(nameof(TokenTemplate), typeof(DataTemplate), typeof(TokenTextBox), new PropertyMetadata(null));
+
+        public TokenTextBox()
+        {
+        }
+
+        public void ReplaceCurrentTextToToken(object token)
+        {
+
+        }
+    }
+}
